43a6400fdd0ac334abbef19889f698cd89c33bee
Ahmad Abou Hashem <abouhashem.ahmad@gmail.com>
2021-02-22 03:08:18 +0200

    all

--- src/Voximplant.js
+++ src/Voximplant.js
@@ -17,18 +17,10 @@ import VideoView from './call/VideoView';
 import EndpointEvents from './call/EndpointEvents';
 import ClientEvents from './client/ClientEvents';
 import {ClientState, CameraType, RenderScaleType, LogLevel, CallError, VideoCodec, RequestAudioFocusMode, VideoStreamType} from "./Enums";
-
-
 import * as Hardware from './hardware';
 import * as Messaging from './messaging';
-import RTCPeerConnection from '../RTCPeerConnection';
-import RTCIceCandidate from '../RTCIceCandidate';
-import RTCSessionDescription from '../RTCSessionDescription';
-import RTCView from '../RTCView';
-import MediaStream from '../MediaStream';
-import MediaStreamTrack from '../MediaStreamTrack';
-import mediaDevices from '../MediaDevices';
-import permissions from '../Permissions';
+
+import * as webrtc from './react-native-webrtc';
 
 /**
  * Get Voximplant.Client instance to use platform functions
@@ -70,36 +62,5 @@ export {
     VideoStreamType,
     Hardware,
     Messaging,
-    RTCPeerConnection,
-    RTCIceCandidate,
-    RTCSessionDescription,
-    RTCView,
-    MediaStream,
-    MediaStreamTrack,
-    mediaDevices,
-    permissions,
-    registerGlobals
+    webrtc
 };
-
-function registerGlobals() {
-	// Should not happen. React Native has a global navigator object.
-	if (typeof navigator !== 'object') {
-		throw new Error('navigator is not an object');
-	}
-
-	if (!navigator.mediaDevices) {
-		navigator.mediaDevices = {};
-	}
-
-	navigator.mediaDevices.getUserMedia =
-		mediaDevices.getUserMedia.bind(mediaDevices);
-
-	navigator.mediaDevices.enumerateDevices =
-		mediaDevices.enumerateDevices.bind(mediaDevices);
-
-	global.RTCPeerConnection     = RTCPeerConnection;
-	global.RTCIceCandidate       = RTCIceCandidate;
-	global.RTCSessionDescription = RTCSessionDescription;
-	global.MediaStream           = MediaStream;
-	global.MediaStreamTrack      = MediaStreamTrack;
-}
